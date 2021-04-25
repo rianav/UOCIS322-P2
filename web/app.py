a57ea474ac9f8b3c116d77d54c11b918a12eff29
Riana Valenzuela <rianav@testium.cs.uoregon.edu>
2021-04-25 14:17:08 -0700

    Complete project

--- web/app.py
+++ web/app.py
@@ -1,14 +1,31 @@
 """
 John Doe's Flask API.
 """
-
-from flask import Flask
+from flask import Flask, render_template, send_from_directory, abort
+import config # Configure from .ini files and command line
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+@app.errorhandler(403)
+def error_403(e):
+    return render_template("403.html")
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template("404.html")
+
+@app.route("/<path:file>")
+def respond(file):
+    docroot = options.DOCROOT
+    #filepath = os.path.join(docroot, file)
+    if ('~' in file) or ('//' in file) or ('..' in file):
+        return abort(403)
+    elif file.endswith('.html' or '.css'):
+        return send_from_directory(docroot, file)
+    else:
+        return abort(404)
 
 if __name__ == "__main__":
+    options = config.configuration()
     app.run(debug=True, host='0.0.0.0')
